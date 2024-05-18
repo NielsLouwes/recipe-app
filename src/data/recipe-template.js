aa069159fd9e539fb9b788eafd4bb68fa25f85e7
Niels Louwes <nielslouwes45@gmail.com>
2024-05-18 15:26:05 +0200

    feat/added-three-recipes-with-data-template

---
+++ src/data/recipe-template.js
@@ -0,0 +1,19 @@
+// template for recipes
+
+const recipeTemplateObject = {
+  id: 1,
+  name: "string",
+  description: "string",
+  ingredients: [],
+  nutrition: [],
+  spices: [],
+  instructions: [],
+  tips: [],
+  "prep-time": null,
+  "cooking-time": 10,
+  serves: "2-3",
+  difficulty: 3,
+  cuisine: "string",
+  calories: null,
+  tags: [],
+};
